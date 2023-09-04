943209222a5b1d3dfff07dd7ae3bc88bf4390b69
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-09-04 21:31:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,26 @@ const App = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+         const fetchData = async () => {
+      try {
+        const response = await axios.get("https://randomuser.me/api/?results=10");
+       
+        const firstNames = response.data.results.map((user) => user.name.first);
+        setItems(firstNames);
+
+        
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+    }, []);
 
     return <Users isLoading={isLoading} items={items} />;
 };
